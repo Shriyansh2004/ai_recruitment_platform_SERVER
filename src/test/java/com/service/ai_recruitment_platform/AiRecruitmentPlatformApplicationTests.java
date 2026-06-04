b7effe0ed4f140a1964b4e11a1371b542bf9c197
Shriyansh@2004 <sanubhab629@gmail.com>
2026-06-04 11:45:44 +0530

    User service added
    Signed-off-by: Shriyansh@2004 <sanubhab629@gmail.com>

---
+++ src/test/java/com/service/ai_recruitment_platform/AiRecruitmentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.ai_recruitment_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecruitmentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
